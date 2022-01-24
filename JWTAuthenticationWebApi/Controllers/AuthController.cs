ee85dcd11c31aa8cb8d281731359499393f3c696
Nikos S <niksal1991@gmail.com>
2022-01-24 20:52:21 +0200

    Register method in controller

---
+++ JWTAuthenticationWebApi/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using JWTAuthenticationWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JWTAuthenticationWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        public static  User user = new User();
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto userDto) {
+            CreatePasswordHash(userDto.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+                user.Username = userDto.Username;
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+                
+            return Ok(user);
+            
+        }
+
+
+        private void CreatePasswordHash(string password,out byte[]passwordHash , out byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
